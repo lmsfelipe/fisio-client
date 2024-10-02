8f65d19cdfb86faae3b4af520691d4be3b2eb2c3
Felipe Lima <lms.felipe@gmail.com>
2024-10-02 20:26:28 -0300

    fix: checks if request is client or server side

--- src/services/request.ts
+++ src/services/request.ts
@@ -2,19 +2,31 @@ import { getCookieAction } from "@/app/actions";
 
 const baseURL = "http://localhost:8000";
 
+function getCookie(key: string) {
+  var b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
+  return b ? b.pop() : "";
+}
+
 async function request<T = unknown, B = unknown>(
   input: RequestInfo | URL,
   init?: RequestInit | undefined,
   payload?: B
 ) {
-  const token = await getCookieAction("jwt-token");
+  let token = null;
+
+  if (typeof window === "undefined") {
+    const cookie = await getCookieAction("jwt-token");
+    token = cookie?.value;
+  } else {
+    token = getCookie("jwt-token");
+  }
 
   const response = await fetch(`${baseURL}/${input}`, {
     ...init,
     ...(payload ? { body: JSON.stringify(payload) } : {}),
     headers: {
       "Content-Type": "application/json", // 'application/x-www-form-urlencoded' if needed,
-      ...(token ? { authorization: `Bearer ${token.value}` } : {}),
+      ...(token ? { authorization: `Bearer ${token}` } : {}),
     },
   });
 
