314cbd18a2bde97a30c01f41d592dde5caa60938
Felipe Lima <lms.felipe@gmail.com>
2024-10-02 20:27:46 -0300

    feat: card styling and create from clicking on calendar

--- src/app/components/AppointmentsColumn.tsx
+++ src/app/components/AppointmentsColumn.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { differenceInMinutes } from "date-fns";
+import { differenceInMinutes, isAfter, parse } from "date-fns";
 import { useMemo } from "react";
-
-import { TAppointment } from "@/common/types";
-import { formatDateWithTimezone, roundedHours } from "@/common/utils";
+import { useSearchParams } from "next/navigation";
 import { parseAsBoolean, parseAsJson, useQueryState } from "nuqs";
+import { getLocalTimeZone, today } from "@internationalized/date";
+
+import { TAppointment, TAppointmentQuery, TSmallAppt } from "@/common/types";
+import { formatDateWithTimezone, roundedHours } from "@/common/utils";
 
 function calculateSize(appointment: TAppointment) {
   const sizes: Record<number, string> = {
@@ -50,9 +52,43 @@ ${formatDateWithTimezone(appt.dateEnd, "hh:mm")}`;
 
 export function AppointmentsColumn({
   appointments,
+  professionalId,
 }: {
   appointments: TAppointment[];
+  professionalId: string;
 }) {
+  const [, setAppointmentQuery] = useQueryState<TAppointmentQuery | TSmallAppt>(
+    "appt",
+    parseAsJson()
+  );
+  const [, setApptModalOpen] = useQueryState("appt-modal", parseAsBoolean);
+
+  const search = useSearchParams();
+
+  const queryDate = search.get("date");
+  const isAfterToday = queryDate ? isAfter(queryDate, new Date()) : false;
+
+  function handleEditAppt(appt: TAppointment | null) {
+    const query = { ...appt, isEdit: true } as TAppointmentQuery;
+    setAppointmentQuery(query);
+    setApptModalOpen(true);
+  }
+
+  function handleCreateAppointment(data: { hour: string }) {
+    const date = queryDate || today(getLocalTimeZone());
+    const parseDate = parse(
+      `${date} ${data.hour}`,
+      "yyyy-MM-dd HH:mm",
+      new Date()
+    );
+
+    setAppointmentQuery({
+      dateStart: parseDate.toISOString(),
+      professionalId,
+    });
+    setApptModalOpen(true);
+  }
+
   const filteredAppointments = (appts: TAppointment[]) =>
     roundedHours.reduce<{ hour: string; data: TAppointment | null }[]>(
       (acc, curr) => {
@@ -74,29 +110,24 @@ export function AppointmentsColumn({
     [appointments]
   );
 
-  const [, setAppointmentQuery] = useQueryState<TAppointment>(
-    "appt",
-    parseAsJson()
-  );
-
-  const [, setApptModalOpen] = useQueryState("appt-modal", parseAsBoolean);
-
-  function handleDate(appt: TAppointment | null) {
-    setAppointmentQuery(appt);
-    setApptModalOpen(true);
-  }
-
   return (
     <div className="appointments-columns">
       {appointmentsMemo.map((appt, index) => (
         <div
           key={`${appt.hour}-${index}`}
-          className="appointments-columns__cell h-32 w-48 border-t border-l-neutral-400 pr-2"
+          className="appointments-columns__cell relative h-32 w-48 border-t border-l-neutral-400 px-1"
         >
+          {isAfterToday && (
+            <div
+              className="h-full w-full absolute left-0 hover:bg-slate-100 cursor-pointer"
+              onClick={() => handleCreateAppointment(appt)}
+            />
+          )}
+
           {appt.data ? (
             <button
-              onClick={() => handleDate(appt.data)}
-              className="appointments-columns__card relative w-full z-10 cursor-pointer"
+              onClick={() => handleEditAppt(appt.data)}
+              className="appointments-columns__card relative w-full z-10 cursor-pointer overflow-hidden"
               style={{
                 height: calculateSize(appt.data),
                 top: calculatePosition(appt.data.dateStart),
@@ -107,9 +138,15 @@ export function AppointmentsColumn({
                   {appt.data.patientName} & {appt.data.professionalName}
                 </div>
 
-                <div className="text-sm text-yellow-700">
+                <div className="text-xs text-yellow-700">
                   {appointmentTime(appt.data)}
                 </div>
+
+                {appt.data.observation ? (
+                  <div className="text-xs text-yellow-900 mt-1">
+                    O paciente deverá ser atendido em conjunto com um auxiliar
+                  </div>
+                ) : null}
               </div>
             </button>
           ) : null}
--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -26,38 +26,50 @@ export default async function Calendar({
   const data = await getData(queryDate);
 
   return (
-    <div className="inline-flex bg-slate-50 rounded-3xl m-6 px-14 py-10">
-      <div className="mr-4">
-        <div className="font-bold h-8 w-6 mb-9 flex items-center">
-          <CalendarDaysIcon />
-        </div>
-
-        {roundedHours.map((hour) => (
-          <div
-            key={uuidv4()}
-            className="text-sm text-slate-800 h-28 w-5 relative"
-          >
-            <div className="clalendar-hour absolute" style={{ bottom: "90%" }}>
-              {hour.split(":")[0]}h
-            </div>
-          </div>
-        ))}
+    <div className="m-6">
+      <div className="text-3xl text-white font-extrabold capitalize mb-4">
+        {formatDateWithTimezone(queryDate, "EEEE, d 'de' MMMM")}
       </div>
 
-      <div className="flex justify-between">
-        {data?.length ? (
-          data.map((professional: TProfessionalWithAppoitments) => (
-            <div key={professional.id}>
-              <div className="mb-8 flex justify-center uppercase">
-                <Avatar color="default" name={professional.name} />
-              </div>
+      <div className="inline-flex bg-slate-50 rounded-3xl px-14 py-10">
+        <div className="mr-4">
+          <div className="font-bold h-8 w-6 mb-9 flex items-center">
+            <CalendarDaysIcon />
+          </div>
 
-              <AppointmentsColumn appointments={professional.appointments} />
+          {roundedHours.map((hour) => (
+            <div
+              key={uuidv4()}
+              className="text-sm text-slate-800 h-32 w-5 relative"
+            >
+              <div
+                className="clalendar-hour absolute"
+                style={{ bottom: "90%" }}
+              >
+                {hour.split(":")[0]}h
+              </div>
             </div>
-          ))
-        ) : (
-          <div>Carregando...</div>
-        )}
+          ))}
+        </div>
+
+        <div className="flex justify-between">
+          {data?.length ? (
+            data.map((professional: TProfessionalWithAppoitments) => (
+              <div key={professional.id}>
+                <div className="mb-8 flex justify-center uppercase">
+                  <Avatar color="default" name={professional.name} />
+                </div>
+
+                <AppointmentsColumn
+                  appointments={professional.appointments}
+                  professionalId={professional.id}
+                />
+              </div>
+            ))
+          ) : (
+            <div>Carregando...</div>
+          )}
+        </div>
       </div>
     </div>
   );
