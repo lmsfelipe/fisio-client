2521b796e4447e9f97a6b26890947af297515f0a
Felipe Lima <lms.felipe@gmail.com>
2024-10-16 19:41:44 -0300

    fix: show two appointments at the same hour

--- src/app/components/AppointmentsColumn.tsx
+++ src/app/components/AppointmentsColumn.tsx
@@ -90,15 +90,15 @@ export function AppointmentsColumn({
   }
 
   const filteredAppointments = (appts: TAppointment[]) =>
-    roundedHours.reduce<{ hour: string; data: TAppointment | null }[]>(
+    roundedHours.reduce<{ hour: string; data: TAppointment[] | [] }[]>(
       (acc, curr) => {
-        const foundAppt = appts.find(
+        const foundAppt = appts.filter(
           (appt) =>
             formatDateWithTimezone(appt.dateStart, "HH") === curr.split(":")[0]
         );
 
-        if (foundAppt) acc.push({ hour: curr, data: foundAppt });
-        else acc.push({ hour: curr, data: null });
+        if (foundAppt.length) acc.push({ hour: curr, data: foundAppt });
+        else acc.push({ hour: curr, data: [] });
 
         return acc;
       },
@@ -124,32 +124,35 @@ export function AppointmentsColumn({
             />
           )}
 
-          {appt.data ? (
-            <button
-              onClick={() => handleEditAppt(appt.data)}
-              className="appointments-columns__card relative w-full z-10 cursor-pointer overflow-hidden"
-              style={{
-                height: calculateSize(appt.data),
-                top: calculatePosition(appt.data.dateStart),
-              }}
-            >
-              <div className="w-full h-full bg-orange-100 border border-orange-300 text-left rounded-2xl px-3 py-2 text-slate-950">
-                <div className="text-sm text-yellow-900 font-bold">
-                  {appt.data.patientName} & {appt.data.professionalName}
-                </div>
+          {appt.data.length
+            ? appt.data.map((item) => (
+                <button
+                  key={item.patientId}
+                  onClick={() => handleEditAppt(item)}
+                  className="appointments-columns__card absolute w-full z-10 cursor-pointer overflow-hidden"
+                  style={{
+                    height: calculateSize(item),
+                    top: calculatePosition(item.dateStart),
+                  }}
+                >
+                  <div className="w-full h-full bg-green-100 border border-green-300 text-left rounded-2xl px-3 py-2 text-slate-950">
+                    <div className="text-sm text-green-900 font-bold">
+                      {item.patientName} & {item.professionalName}
+                    </div>
 
-                <div className="text-xs text-yellow-700">
-                  {appointmentTime(appt.data)}
-                </div>
+                    <div className="text-xs text-green-700">
+                      {appointmentTime(item)}
+                    </div>
 
-                {appt.data.observation ? (
-                  <div className="text-xs text-yellow-900 mt-1">
-                    O paciente deverá ser atendido em conjunto com um auxiliar
+                    {item.observation ? (
+                      <div className="text-xs text-green-900 mt-1">
+                        {item.observation}
+                      </div>
+                    ) : null}
                   </div>
-                ) : null}
-              </div>
-            </button>
-          ) : null}
+                </button>
+              ))
+            : null}
         </div>
       ))}
     </div>
